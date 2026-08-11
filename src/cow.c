a5a87a66861a75650d6a65acc439c791f1a1df19
ShaGhay3Gh <shaghay3gh@gmail.com>
2026-08-12 00:38:43 +0200

    I have a cow

--- src/cow.c
+++ src/cow.c
@@ -3,5 +3,11 @@
 
 void print_cow(void)
 {
-    printf("[Cow will appear here]\n");
+    // printf("[Cow will appear here]\n");
+    printf("\\   ^__^\n");
+    printf(" \\  (oo)\\_______\n");
+    printf("    (__)\\       )\\/\\\n");
+    printf("        ||----w |\n");
+    printf("        ||     ||\n");
+
 }
