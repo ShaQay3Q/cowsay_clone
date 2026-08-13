fc6813068b66ecbf965ff391f9c782f0970792f6
ShaGhay3Gh <shaghay3gh@gmail.com>
2026-08-13 17:22:12 +0200

    working on speech bubble

--- src/main.c
+++ src/main.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 #include "./cow.h"
+#include <string.h>
+
+int count_length(char *input[]);
+void print_dash(int length);
+
+
 
 int main(int argc, char *argv[])
 {
@@ -9,7 +15,24 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    print_dash(count_length(&argv[1]));
     printf("< %s >\n", argv[1]);
+    print_dash(count_length(&argv[1]));
     print_cow();
     return 0;
+}
+
+int count_length(char *input[])
+{
+    return strlen(*input);
+}
+
+void print_dash(int length)
+{
+    printf(" ");
+    for (int i = 0; i < length + 2; i++)
+    {
+        printf("-");
+    }
+    printf("\n");
 }
